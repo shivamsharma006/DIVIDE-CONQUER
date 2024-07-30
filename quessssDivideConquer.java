9bb74491065898ccd312cfe7bb13c53c4e089632
shivamsharma006 <147228448+shivamsharma006@users.noreply.github.com>
2024-07-30 23:58:48 +0530

    Add files via upload

---
+++ quessssDivideConquer.java
@@ -0,0 +1,57 @@
+ /*
+ Q5. An interval is represented as a combination of start time
+and end time. 
+Given a set of intervals, check if any two intervals intersect.
+
+Input: arr[] = {{1, 3}, {5, 7}, {2, 4}, {6, 8}} 
+Output: Yes 
+The intervals {1, 3} and {2, 4} overlap 
+
+Input: arr[] = {{1, 3}, {7, 9}, {4, 6}, {10, 13}} 
+Output: No
+ */
+
+public class quessssDivideConquer {
+
+    static class Interval{
+        int start;
+        int end;
+        public Interval(int start,int end){
+            this.start = start;
+            this.end = end;
+        }
+    }
+    public static void main(String[] args) {
+        Interval arr[] = {new Interval(1, 3), new Interval(7, 9),new Interval(4, 6),new Interval(10, 13)};
+        int n1 = arr.length;
+        
+     	if(isIntersect(arr, n1))
+        System.out.print("Yes\n");
+        else
+        System.out.print("No\n");
+    }
+
+    public static boolean isIntersect(Interval[]arr,int n1){
+        int max = 0;
+        for(int i=0; i<n1; i++){
+            if(max<arr[i].end){
+                max = arr[i].end;
+            }
+        }
+        int[]aux = new int[max+1];
+        for(int i=0; i<n1; i++){
+            int x = arr[i].start;
+            int y = arr[i].end;
+            aux[x]++;
+            aux[y]--;
+        }
+
+        for(int i=1; i<n1; i++){
+            aux[i] += aux[i-1];
+            if(aux[i]>1){
+                return true;
+            }
+        }
+        return false;
+    }
+}
